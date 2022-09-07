3f910c20daae6de7f495544af453aea9ce1d8fd7
GeorgeMoucht <georgemoucht@gmail.com>
2022-09-07 18:21:30 +0300

    new UI version

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -36,8 +36,11 @@
             </li> -->
             </ul>
         </div>
-        </nav>
+    </nav>
+
+    <div class="container">
         {{content}}
+    </div>
         
     <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>

f448277c84495d3cfdc6463ae65502f091a6c4f0
GeorgeMoucht <georgemoucht@gmail.com>
2022-09-07 18:21:48 +0300

    v1 of Contact form UI

--- views/contact.php
+++ views/contact.php
@@ -1,1 +1,19 @@
 <h1>Contact</h1>
+
+<form action="" method="post">
+    <div class="form-group">
+        <label>Subject</label>
+        <input type="text" name="subject" class="form-control">
+    </div>
+    <div class="form-group">
+        <label>Email address</label>
+        <input type="email" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <label>Body</label>
+        <textarea class="form-control"></textarea> 
+    </div>
+    <br>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>

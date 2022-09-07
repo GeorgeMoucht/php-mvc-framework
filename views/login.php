dfcc01d98ca4aea6bf916911696ccedc6e4ad3ab
GeorgeMoucht <georgemoucht@gmail.com>
2022-09-07 19:59:06 +0300

    v1 of UI

---
+++ views/login.php
@@ -0,0 +1,16 @@
+<h1>Login</h1>
+
+<form action="" method="post">
+    <div class="form-group">
+        <label>Email address</label>
+        <input type="email" name="email" class="form-control" placeholder="Enter your email">
+    </div>
+
+    <div class="form-group">
+        <label>Password</label>
+        <input type="password" name="password" class="form-control" placeholder="Enter your password">
+
+    </div>
+    <br>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
---
+++ views/register.php
@@ -0,0 +1,34 @@
+<h1>Create your Account</h1>
+
+<form action="" method="post">
+    <div class="row">
+        <div class="col">
+           <div class="form-group">
+                <label>Firstname</label>
+                <input type="text" name="firstname" class="form-control" placeholder="Enter your firstname">
+            </div>
+        </div>
+        <div class="col">
+            <div class="form-group">
+                <label>Lastname</label>
+                <input type="text" name="lastname" class="form-control" placeholder="Enter your email">
+            </div>  
+        </div>
+    </div>
+
+    <div class="form-group">
+        <label>Email address</label>
+        <input type="email" name="email" class="form-control" placeholder="Enter your email">
+    </div>
+
+    <div class="form-group">
+        <label>Password</label>
+        <input type="password" name="password" class="form-control" placeholder="Enter your password">
+    </div>
+    <div class="form-group">
+        <label>Confirm Password</label>
+        <input type="password" name="confirmPassword" class="form-control" placeholder="Repeat your password">
+    </div>
+    <br>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>

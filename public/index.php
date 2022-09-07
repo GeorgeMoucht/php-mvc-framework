b03b80994e2cf5b0009020af9c7b25fe6c1bf42f
GeorgeMoucht <georgemoucht@gmail.com>
2022-09-07 15:12:23 +0300

    Move index inside public directory

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 //First runtime script by users.
 //All requests will be handled by this script.
 
-require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 use app\core\Application;
 
 ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);

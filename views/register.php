eaa04c183b100c2826f1164ff5e66fbff8323711
GeorgeMoucht <georgemoucht@gmail.com>
2022-09-09 20:20:48 +0300

    recreate register form with php

--- views/register.php
+++ views/register.php
@@ -1,39 +1,18 @@
 <h1>Create your Account</h1>
 
-<?php
-    // echo "<pre>";
-    // var_dump($errors);
-    // echo "</pre>";
-?>
-<form action="" method="post">
+<?php $form = \app\core\form\Form::begin('', 'post'); ?>
     <div class="row">
         <div class="col">
-           <div class="form-group">
-                <label>Firstname</label>
-                <input type="text" name="firstname" class="form-control" placeholder="Enter your firstname">
-            </div>
+            <?php echo $form->field($model , 'firstname') ?>
         </div>
         <div class="col">
-            <div class="form-group">
-                <label>Lastname</label>
-                <input type="text" name="lastname" class="form-control" placeholder="Enter your email">
-            </div>  
+            <?php echo $form->field($model , 'lastname') ?>
         </div>
     </div>
+    <?php echo $form->field($model , 'email') ?>
+    <?php echo $form->field($model , 'password')->passwordField() ?>
+    <?php echo $form->field($model , 'confirmPassword')->passwordField() ?>
 
-    <div class="form-group">
-        <label>Email address</label>
-        <input type="email" name="email" class="form-control" placeholder="Enter your email">
-    </div>
-
-    <div class="form-group">
-        <label>Password</label>
-        <input type="password" name="password" class="form-control" placeholder="Enter your password">
-    </div>
-    <div class="form-group">
-        <label>Confirm Password</label>
-        <input type="password" name="confirmPassword" class="form-control" placeholder="Repeat your password">
-    </div>
-    <br>
     <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+
+<?php \app\core\form\Form::end(); ?>

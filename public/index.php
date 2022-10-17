b328e751d1c04ba95ef794742235d486743d3870
GeorgeMoucht <georgemoucht@gmail.com>
2022-10-17 22:26:21 +0300

    implement createImmutable() that load data from .env file

--- public/index.php
+++ public/index.php
@@ -2,15 +2,29 @@
 //First runtime script by users.
 //All requests will be handled by this script.
 
-require_once __DIR__.'/../vendor/autoload.php';
-
 use app\controllers\SiteController;
 use app\core\Application;
 use app\controllers\AuthController;
 
+require_once __DIR__.'/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+
+// echo phpinfo();
+// exit;
+
 ini_set('display_errors', 1); error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);;
 
-$app = new Application(dirname(__DIR__));   //create the application and pass project root directory
+$config = [
+    'db' => [ // Values dsn/user/password comming from the .env file using vlucas/phpdotenv package.
+        'dsn' => $_ENV['DB_DSN'],
+        'user' => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASSWORD'],
+    ]
+];
+
+$app = new Application(dirname(__DIR__), $config);   //create the application and pass project root directory
 
 //Map all the URL's that we have in our application.
 

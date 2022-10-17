da65027b348ea5da25f15a53f8da7286bc818057
GeorgeMoucht <georgemoucht@gmail.com>
2022-10-17 22:51:15 +0300

    initial database class into the application

--- core/Application.php
+++ core/Application.php
@@ -17,17 +17,19 @@ class Application
     public static string $ROOT_DIR;
     public Router $router;
     public Response $response;
+    public Database $db;
     public Request $request;
     public Controller $controller;
     public static Application $app;
 
-    public function __construct($rootPath)
+    public function __construct($rootPath, array $config)
     {
         self::$ROOT_DIR = $rootPath;
         self::$app = $this;
         $this->request = new Request();
         $this->response = new Response();
         $this->router = new Router($this->request, $this->response);
+        $this->db = new Database($config['db']);
     }
 
 

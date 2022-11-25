0b1a36302dda34092fb871f19d1448065a3e47a1
GeorgeMoucht <giorgosmoucht@gmail.com>
2022-11-25 21:31:35 +0200

    re-write login form with php

--- views/login.php
+++ views/login.php
@@ -1,4 +1,4 @@
-<h1>Login</h1>
+<!-- <h1>Login</h1>
 
 <form action="" method="post">
     <div class="form-group">
@@ -13,4 +13,18 @@
     </div>
     <br>
     <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+</form> -->
+
+<?php
+/** @var $model \app\models\User  */
+?>
+
+<h1>Login</h1>
+
+<?php $form = \app\core\form\Form::begin('', 'post'); ?>
+    <?php echo $form->field($model , 'email') ?>
+    <?php echo $form->field($model , 'password')->passwordField() ?>
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+
+<?php \app\core\form\Form::end(); ?>

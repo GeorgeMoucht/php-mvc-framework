ae0755069c1751eab4b18f5de669c5579560f1a8
geortrim <trimisg5@gmail.com>
2022-12-28 20:07:20 +0200

    create media query for the forms in login and register

--- views/login.php
+++ views/login.php
@@ -9,8 +9,8 @@
         <img src="/assets/img/example-logo.png">
     </div>
     <div class="flx-wrapper">
-        <div class="login-block block">
-            <div class="third-party-login-cnt">
+        <div class="form-block block">
+            <div class="third-party-cnt">
                 <h2>NAME OF THE SITE</h2>
                 <div class="underline"></div>
 
@@ -19,9 +19,13 @@
                     <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l7-7z"/>
                 </svg>
                 <span class="white-text">Cookies must be enabled in your browser.</span>
+                <br><br>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio ratione vero provident reiciendis, sint autem distinctio veniam optio ex error molestias architecto dolor dicta non nemo nesciunt est suscipit pariatur.
+                </p>    
                 <h3>Log in using your account on:</h3>
 
-                <button class="google-login">
+                <button class="google-btn">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-google" viewBox="0 0 16 16">
                         <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z"/>
                     </svg>
